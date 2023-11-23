1984c0af567583b644e32d708338f427b7fe494a
laura-barrera-valdes <laurabv99@gmail.com>
2023-11-22 19:36:59 -0500

    feat: maze and photo screens without camera permissions

--- Screens/maze/maze.js
+++ Screens/maze/maze.js
@@ -6,11 +6,11 @@ function setup() {
 function draw() {}
 
 function backMain() {
-  window.location.href = "../main-menu/main-menu.html";
+  window.location.href = "../take-photo/take-photo.html";
 }
 
 function movePlay() {
-  window.location.href = "../maze/maze.html";
+  window.location.href = "../../player1/index.html";
 }
 
 function createElements() {
@@ -25,19 +25,14 @@ function createElements() {
   // Crear elementos del encabezado
   let header = createDiv("");
   header.class("head");
-  let title = createElement("h1", "Photo");
-  let number = createElement(
-    "p",
-    "Take a photo to identify your self in the competition."
-  );
+  let title = createElement("h1", "Maze");
+  let number = createElement("p", "The first to reach the goal, wins.");
 
   // Crear elemento de imagen
   let imageNumber = createDiv("");
   imageNumber.class("photo");
-  let profileImage = createImg(
-    "https://vivolabs.es/wp-content/uploads/2022/03/perfil-hombre-vivo.png",
-    ""
-  );
+  let mapimg = createImg("../imgs/map.png", "Rappi Logo");
+
 
   // Agregar elementos al DOM
   document.body.appendChild(backButton.elt);
@@ -45,22 +40,17 @@ function createElements() {
   header.child(title);
   header.child(number);
   document.body.appendChild(imageNumber.elt);
-  imageNumber.child(profileImage);
+  imageNumber.child(mapimg);
 
   let menuDiv = createDiv("");
   menuDiv.class("menu-buttons");
 
-  let startButton = createButton("Take a Photo");
+  let startButton = createButton("Next");
   startButton.mousePressed(movePlay);
   startButton.class("buttonPhoto");
 
-  let playersScoreButton = createButton("Select from Gallery");
-  playersScoreButton.mousePressed(movePlay);
-  playersScoreButton.class("buttonPhoto");
-
   //menuDiv.parent("body");
   startButton.parent(menuDiv);
-  playersScoreButton.parent(menuDiv);
 
   let logoDiv = createDiv("");
   logoDiv.class("logo-div");
